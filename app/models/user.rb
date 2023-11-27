8ad4270d6a91943ab713c83c0f51dc7769aba375
DONSAN7 <148339936+DONSAN7@users.noreply.github.com>
2023-11-27 01:55:49 +0000

    [update]フォロー・フォロワー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,34 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # フォローしている関連付け
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローしているユーザーを取得
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォロワーを取得
+  has_many :followings, through: :active_relationships, source: :followed
+  # 指定したユーザーをフォローする
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+  # 指定したユーザーをフォローする
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+
+  # 指定したユーザーのフォローを解除する
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  # 指定したユーザーをフォローしているかどうかを判定
+  def following?(user)
+    followings.include?(user)
+  end
 
   def get_profile_image(width, height)
     unless profile_image.attached?
